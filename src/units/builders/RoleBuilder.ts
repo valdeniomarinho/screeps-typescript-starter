9a89a05d058edfcf86bf22c7a40b00aeef1293d4
valdeniomarinho <valdeniomarinho@pm.me>
2021-07-26 19:37:53 -0300

    update and restructuring

--- src/units/builders/RoleBuilder.ts
+++ src/units/builders/RoleBuilder.ts
@@ -5,21 +5,42 @@ export default class RoleBuilder {
   public static active = false
   public static total = 0
   public static source = 0
-  public static model: BodyPartConstant[] = [WORK, CARRY, MOVE]
+  public static model: BodyPartConstant[] = [
+    WORK,
+    CARRY,
+    MOVE
+  ]
 
   public static get current(): number {
-    const current_harvesters = _.filter(Game.creeps, creep => creep.memory.role === "builder")
+    const current_harvesters = _.filter(
+      Game.creeps,
+      creep => creep.memory.role === "builder"
+    )
     return current_harvesters.length
   }
 
-  public static run(creep: Creep, restpoint: string): void {
-    const hasConstructionSites = creep.room.find(FIND_CONSTRUCTION_SITES)
+  public static run(
+    creep: Creep,
+    restpoint: string
+  ): void {
+    const hasConstructionSites = creep.room.find(
+      FIND_CONSTRUCTION_SITES
+    )
 
-    if (this.active && hasConstructionSites.length) {
-      if (creep.memory.building && creep.store[RESOURCE_ENERGY] === 0) {
+    if (
+      this.active &&
+      hasConstructionSites.length
+    ) {
+      if (
+        creep.memory.building &&
+        creep.store[RESOURCE_ENERGY] === 0
+      ) {
         creep.memory.building = false
       }
-      if (!creep.memory.building && creep.store.getFreeCapacity() === 0) {
+      if (
+        !creep.memory.building &&
+        creep.store.getFreeCapacity() === 0
+      ) {
         creep.memory.building = true
       }
 
